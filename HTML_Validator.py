35d6dc4b7a5aaf864314b406fd18bc32ab1f6285
benfig1127 <benjifig@gmail.com>
2021-01-21 18:37:30 -0800

    Added full functionality of the main function

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -1,29 +1,63 @@
 #!/bin/python3
 
-from pythonds.basic import Stack
-
 def validate_html(html):
 	
-	'''
-	This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
-
-	>>> validate_html('<strong>example</strong>')
-	True
-	>>> validate_html('<strong>example')
-	False
-	'''
-
-	# HINT:
-	# use the _extract_tags function below to generate a list of html tags without any extra text;
-	# then process these html tags using the balanced parentheses algorithm from the book
-	# the main difference between your code and the book's code will be that you will have to keep track of not just the 3 types of parentheses,
-	# but arbitrary text located between the html tags
-
-	return output
+	#get the html tags with our helper function
+	html_tags=_extract_tags(html)
 	
+	#encode all the tags as either A or B, where A is the opening tag and B is the closing tag
+	encoding_list=[]
 	
+	for index, tag in enumerate(html_tags):
+		
+		for index_2, char in enumerate(tag):
+			#check for '<' and encode as A
+			if tag[index_2]=='<' and tag[index_2+1]!='/':
+				encoding_list.append('A')
+			#check for '</' and encode as B
+			if tag[index_2]=='<' and tag[index_2+1]=='/':
+				encoding_list.append('B')
+			
 	
-
+	print('html tags:',html_tags)
+	print('encodings:',encoding_list)
+	
+	stack=[]
+	balanced=True
+	index=0
+	
+	while index < len(encoding_list) and balanced:
+		
+		#grab the encoding 
+		item=encoding_list[index]
+		
+		#if the encoding is an opening tag we push it to the stack 
+		if item in 'A':
+			#push item to the stack 
+			stack.append(item)
+		else:
+			#check to see if the stack is empty 
+			
+			#returns true if we are attempting to add the encoding B and the stack doesnt have anything on it, which by definition means there is no matching A before the B, thus it is not balanced 
+			if not stack:
+				balanced=False
+				
+			#if the stack is non empty then we pop the top stack item and inspect to see if it is an A encoding in order to match our held B encoding
+			else:
+				top_item=stack.pop()
+				
+				#get the boolean output of whether they match and if they do match, do nothing, if they dont match then change balanced to False
+				if not (top_item=='A' and item=='B'):
+					balanced=False
+		
+		#incriment out index 
+		index+=1
+		
+	#checks to see if the stack is empty and if balanced=True
+	if balanced and (not stack):
+		return True
+	else: 
+		return False
 
 
 def _extract_tags(html):
@@ -76,6 +110,9 @@ def _extract_tags(html):
 
 
 
+
+print(validate_html('<strong>example<strong>example<strong>example</strong>'))
+print(validate_html('<strong>example'))
 
 
 
